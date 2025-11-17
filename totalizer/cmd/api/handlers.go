9034b8fbb45baab6c66e3b6b9ced9ae0c0f0d408
Ahmed Elhady <ahmedelhady0321@gmail.com>
2025-11-17 11:47:44 +0200

    Added simple sum result functionality

--- totalizer/cmd/api/handlers.go
+++ totalizer/cmd/api/handlers.go
@@ -1,7 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/aelhady03/sumflow/totalizer/internal/data"
+)
+
+// healthcheckHandler returns a simple status message to indicate that the API is running.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
 	env := envelope{
@@ -13,6 +18,18 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+// getResultHandler returns a simple sum result.
+func (app *application) getResultHandler(w http.ResponseWriter, r *http.Request) {
+
+	result := data.Sum{
+		Total: 10,
+	}
+	err := app.writeJSON(w, http.StatusOK, envelope{"result": result}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
---
+++ totalizer/internal/data/sum.go
@@ -0,0 +1,6 @@
+package data
+
+// Sum represents the sum result.
+type Sum struct {
+	Total int `json:"total"`
+}
